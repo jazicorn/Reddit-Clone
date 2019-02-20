0c9091a2c6ddeb9246b3c57d8773d37bac6d886a
Jasmine Anderson <jazz2900@gmail.com>
2019-02-20 11:52:31 -0800

    Users can log in

--- controllers/auth.js
+++ controllers/auth.js
@@ -34,12 +34,12 @@ module.exports = app => {
   
   // SIGN UP POST
   app.post("/sign-up", (req, res) => {
-    // Create User
+    // Create User and JWT
     const user = new User(req.body);
 
     user
       .save()
-      .then(user => {
+      .then((user) => {
         var token = jwt.sign({ _id: user._id}, process.env.SECRET, { expiresIn: "60 days"});
         res.cookie('nToken', token, { maxAge: 900000, httpOnly: true})
         res.redirect("/");
@@ -54,5 +54,42 @@ module.exports = app => {
     res.clearCookie('nToken',
     res.redirect('/'))
   })
+
+  // LOGIN
+
+  app.post("/login", (req, res) => {
+    const username =  req.body.username;
+    const password = req.body.password;
+
+    // LOGIN
+    app.post("/login", (req, res) => {
+      const username = req.body.username;
+      const password = req.body.password;
+      // Find this user name
+      User.findOne({ username }, "username password")
+        .then(user => {
+          if (!user) {
+            // User not found
+            return res.status(401).send({ message: "Wrong Username or Password" });
+          }
+          // Check the password
+          user.comparePassword(password, (err, isMatch) => {
+            if (!isMatch) {
+              // Password does not match
+              return res.status(401).send({ message: "Wrong Username or password" });
+            }
+            // Create a token
+            const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, {
+              expiresIn: "60 days"
+            });
+            // Set a cookie and redirect to root
+            res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
+            res.redirect("/");
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+});
 
 };
